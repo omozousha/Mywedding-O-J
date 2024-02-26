52ba011c8480192a118cb4e28a4beac587f9382a
omozousha <hottiopan@raharja.info>
2024-02-26 13:22:26 +0700

    update userDisplay

--- src/components/UserDisplay.js
+++ src/components/UserDisplay.js
@@ -1,13 +1,20 @@
+// UserDisplay.js
+
 import React, { useEffect } from 'react';
 
 const UserDisplay = () => {
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
-        const username = urlParams.get('username');
+        const encodedUsername = urlParams.get('username');
+        const decodedUsername = encodedUsername ? decodeURIComponent(encodedUsername) : null;
         const usernameTypewriter = document.getElementById('username-typewriter');
 
-        if (username) {
-            usernameTypewriter.textContent = username;
+        if (decodedUsername) {
+            // Split the username into two words
+            const [firstName, lastName] = decodedUsername.split(' ');
+
+            // Display the two words
+            usernameTypewriter.textContent = `${firstName} ${lastName}`;
         } else {
             usernameTypewriter.textContent = 'We invite you to celebrate our wedding';
         }
